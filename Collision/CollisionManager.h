27f92d373f0f6bcee100dedf52ef1e03168b94d4
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-14 18:29:36 +0900

    GetNewMaskを追加

--- Collision/CollisionManager.h
+++ Collision/CollisionManager.h
@@ -8,13 +8,47 @@
 class CollisionManager
 {
 public:
+    CollisionManager(const CollisionManager&) = delete;
+    CollisionManager(const CollisionManager&&) = delete;
+    CollisionManager& operator=(const CollisionManager&) = delete;
+    CollisionManager& operator=(const CollisionManager&&) = delete;
+
+    static CollisionManager* GetInstance() { static CollisionManager instance; return &instance; }
+
     void Initialize();
     void CheckAllCollision();
     void RegisterCollider(Collider* _collider);
     void ClearColliderList();
     int32_t GetNewAttribute(std::string _id);
+    template<typename... Args>
+    int32_t GetNewMask(std::string _id, Args... _ignoreNames)
+    {
+        uint32_t result = 0;
+        for (auto& attributePair : attributeList_)
+        {
+            if (_id.compare(attributePair.first) == 0)
+            {
+                result = ~attributePair.second;
+            }
+        }
+        for (std::string name : std::initializer_list<std::string>(_ignoreNames))
+        {
+            for (auto& attributePair : attributeList_)
+            {
+                if (name.compare(attributePair.first) == 0)
+                {
+                    result ^= attributePair.second;
+                }
+            }
+        }
+
+        return result;
+    }
+
 
 private:
+    CollisionManager() = default;
+
     std::vector<Collider*> colliders_;
     std::vector<std::pair<std::string, std::string>> collisionNames_;
     std::vector<std::pair<std::string, int32_t>> attributeList_;
