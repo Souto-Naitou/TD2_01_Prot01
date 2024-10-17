90ddfed2c731c62f170388760f30f54447bb6e62
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-18 08:30:44 +0900

    Enemyにコメントを追加

--- Object/Enemy/Enemy.cpp
+++ Object/Enemy/Enemy.cpp
@@ -11,50 +11,72 @@
 
 Enemy::~Enemy()
 {
-    CollisionManager::GetInstance()->DeleteCollider(&collider_);
+    pCollisionManager->DeleteCollider(&collider_);
     DebugManager::GetInstance()->DeleteComponent("Enemy", idx_.c_str());
 }
 
 void Enemy::Initialize(size_t idx)
 {
-    CollisionManager* pCollisionManager = CollisionManager::GetInstance();
-    idx_ = std::to_string(idx);
-    objectID_ = "Enemy" + idx_;
-    DebugManager::GetInstance()->SetComponent("Enemy", std::to_string(idx), std::bind(&Enemy::DebugWindow, this));
-    keys_ = InputCenter::GetInstance()->GetKeyPtr();
-    preKeys_ = InputCenter::GetInstance()->GetKeyPtr();
-
-    radius_ = 20.0f;
-    ellipseAB_ = { 20.0f ,10.0f };
-    collider_.SetColliderID("Enemy");
-    moveSpeed_ = 1.0f;
-
+    /// コライダーの登録
+    pCollisionManager = CollisionManager::GetInstance();
     pCollisionManager->RegisterCollider(&collider_);
+
+
+    /// デバッグ用ウィンドウの登録
+    idx_ = std::to_string(idx);        // 引数から受け取った数を文字に
+    objectID_ = "Enemy" + idx_;             //"Enemy1", "Enemy2" など
+    DebugManager::GetInstance()->SetComponent("Enemy", idx_, std::bind(&Enemy::DebugWindow, this));
+
+
+    /// キー情報ポインタの取得
+    keys_       = InputCenter::GetInstance()->GetKeyPtr();
+    preKeys_    = InputCenter::GetInstance()->GetKeyPtr();
+
+
+    /// 頂点の計算に必要なデータを入力
+    radius_ = 20.0f;
+    ellipseAB_ = { 20.0f ,10.0f };          // ax^2 + by^2 = 1
+
+
+    collider_.SetColliderID("Enemy");   // コライダーのID
+    moveSpeed_ = 1.0f;                      // 移動スピード
+
+
+    /// コライダーの設定
     collider_.SetOwner(this);
     collider_.SetAttribute(pCollisionManager->GetNewAttribute("Enemy"));
-    //Colliderにポインタを渡す
+    // Colliderにポインタを渡す
     collider_.SetOnCollision(std::bind(&Enemy::OnCollision, this, std::placeholders::_1));
 }
 
 void Enemy::Update()
 {
-    // 衝突後の動き
-    if (isBouncing_) {
+    /// 衝突後の動き
+    if (isBouncing_)
+    {
         // 反発する方向に移動
         position_ += distanceToTarget.Normalize() * bounceSpeed_;
     }
-    else {
-        // 通常の移動処理
+    else
+    {
+        /// 通常の移動処理
         distanceToTarget = positionTarget_ - position_;
-        if (distanceToTarget.Length() > 0) {
+        if (distanceToTarget.Length() > 0)
+        {
             position_ += distanceToTarget.Normalize() * moveSpeed_;
         }
 
-        if (distanceToTarget.x != 0 || distanceToTarget.y != 0) {
+        if (distanceToTarget.x != 0 || distanceToTarget.y != 0)
+        {
             rotation_ = std::atan2(distanceToTarget.y, distanceToTarget.x);
         }
     }
-    /// ここより下ではPositionを更新しない
+
+
+    /// ** ここより下ではPositionを更新しない **
+
+
+    /// 頂点の計算
     float theta = 0;
     Vector2 result = {};
     for (int i = 0; i < 3; i += 1)
@@ -64,14 +86,15 @@ void Enemy::Update()
         result.y = ellipseAB_.y * std::sinf(theta);
         vertices_[i] = result.Rotated(rotation_) + position_;
     }
-
     collider_.SetVertices(vertices_, 3);
+
 
     return;
 }
 
 void Enemy::Draw()
 {
+    /// 3頂点の描画 (Enemy本体)
     Novice::DrawTriangle(
         static_cast<int>(vertices_[0].x), static_cast<int>(vertices_[0].y),
         static_cast<int>(vertices_[1].x), static_cast<int>(vertices_[1].y),
@@ -89,6 +112,7 @@ void Enemy::DebugWindow()
     {
         ImGuiTemplate::VariableTableRow("Position", position_);
         ImGuiTemplate::VariableTableRow("DistanceToTarget", distanceToTarget);
+        ImGuiTemplate::VariableTableRow("isDead_", isDead_);
         ImGuiTemplate::VariableTableRow("rotation_", rotation_);
         ImGuiTemplate::VariableTableRow("Attribute", collider_.GetCollisionAttribute());
         ImGuiTemplate::VariableTableRow("Mask", collider_.GetCollisionMask());
@@ -102,23 +126,29 @@ void Enemy::DebugWindow()
 }
 
 void Enemy::OnCollision(const Collider* _other) {
-    // _otherがPlayerかどうか確認
-    if (_other->GetColliderID() == "Player") {
-        if (!hasCollided_) {
-            // 初回の衝突時
-              // 向き反転
-            distanceToTarget = -distanceToTarget;
-            rotation_ += 3.141592f;
-            //ぶっ飛びフラグオン
-            isBouncing_ = true;
-            //衝突フラグオン
-            hasCollided_ = true;
+
+    /// Playerとの当たり判定
+    if (_other->GetColliderID() == "Player")
+    {
+        if (!hasCollided_)                  // 初回の衝突時
+        {
+            /// 向き反転
+            distanceToTarget    = -distanceToTarget;
+            rotation_           += 3.141592f;
+
+            isBouncing_         = true;     //ぶっ飛びフラグオン
+            hasCollided_        = true;     //衝突フラグオン
         }
-        // 二回目以降の衝突は無視
+
+        /// 二回目以降の衝突は無視
+
     }
-    //コアとの当たり判定
-    else if (_other->GetColliderID()== "Core") {
-        //エネミーのデスフラグをオンに
+
+
+    /// Coreとの当たり判定
+    else if (_other->GetColliderID()== "Core")
+    {
+        // エネミーのデスフラグをオンに
         isDead_ = true;
     }
 }
